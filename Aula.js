a195c50e0c670f30b2dfe46badddfc2b578d71e7
ALberto Mavungo <pablo.silva23@fatec.sp.gov.br>
2022-02-25 19:07:05 -0300

    Primeiro Commit

--- Aula.js
+++ Aula.js
@@ -1,3 +1,42 @@
+const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
+
+//arrow function
+//() => {}
+
+const apenasA = nomes.filter(n) => {
+    return nomes.startsWith("A")
+}
+
+console.log(apenasA)
+
+
+
+/*const numeros = [1, 2]
+console.log(numeros.length)
+numeros[4] = 2
+console.log(numeros.length)*/
+
+/*const numeros = [1, 2]
+numeros[0] = 2
+console.log(numeros)
+numeros = [1, 2]
+const nomes = ['Ana Maria']*/
+
+// int  v [10];
+
+/*const v1 = []
+console.log(v1.length)
+
+v1[0] = 3.4
+v1[10] = 2
+v1[2] = true
+console.log(v1.length)
+for (let i = 0; i < v1.length; i++){
+    console.log(v1[i]) //aqui traz todas as posicoes, incluindo as que estao vazias
+}
+
+v1.forEach((a) => console.log (a)) //aqui traz so as posicoes do vetor que tem alguma coisa*/
+
 /*console.log (1 == 1)
 console.log (1 == "1")
 console.log (1 === "1")
