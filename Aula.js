d9ab80e1f8e9606db1ce01026bd72d411755d3c3
ALberto Mavungo <pablo.silva23@fatec.sp.gov.br>
2022-02-25 18:31:48 -0300

    Primeiro Commit

---
+++ Aula.js
@@ -0,0 +1,43 @@
+/*console.log (1 == 1)
+console.log (1 == "1")
+console.log (1 === "1")
+console.log (true == 1)
+console.log (1 == [1])
+console.log (null == null)
+console.log (null == undefined)
+console.log ([] == false)
+console.log ([] == [])
+const a = []
+const b = a
+console.log (a == b)
+const nome = "a"
+console.log (nome !== "")*/
+
+/*const n1 = 2
+const n2 = '3'
+const n3 = n1 + n2
+console.log(n3)*/
+
+/*var idade = 18
+var a = 3
+var idade = 18
+console.log("Oi. " + nome)
+if (idade >= 18){
+    var nome = "Joao";
+    //console.log ("Parabens, " + nome ", voce pode dirigir.")
+    console.log (`Parabens, ${nome}. Você pode dirigir.`)
+}
+console.log ("Até mais, " + nome)*/
+
+//Tipos de variaveis
+/*const nome = "José"
+const idade = 27
+nome = "Maria"
+const a **/
+
+/*let a = 2
+let b = 'abc'
+let endereco = `Rua K, 12`*/
+
+/*var c = 2 + 3
+var d = "abcd"*/
